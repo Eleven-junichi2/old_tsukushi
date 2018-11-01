9f100d1a15a53debb363937df3c37e658075c8b6
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2018-11-01 21:08:42 +0900

    add images

--- runboxpy/main.py
+++ runboxpy/main.py
@@ -1,29 +1,40 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
 from kivy.config import Config
 
 Config.set('graphics', 'width', '620')
 Config.set('graphics', 'height', '440')
 
 
+class IconButton(Button):
+    icon = StringProperty("")
+
+
+class EditScreen(Screen):
+    pass
+
+
 class WelcomeScreen(Screen):
     pass
 
 
-class RunBoxPySM(ScreenManager):
+class RunboxpySM(ScreenManager):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.add_widget(WelcomeScreen(name="welcome"))
+        self.add_widget(EditScreen(name="edit"))
 
 
-class RunBoxPyApp(App):
+class RunboxpyApp(App):
     def build(self):
-        self.screen_manager = RunBoxPySM()
+        self.screen_manager = RunboxpySM()
         return self.screen_manager
 
 
 def main():
-    return RunBoxPyApp().run()
+    return RunboxpyApp().run()
 
 
 if __name__ == '__main__':
