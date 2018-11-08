4913ede8b47d9025739ddc48a5cc27770f6e43d5
Junichi Suetsugu <junichinomori@yahoo.co.jp>
2018-11-08 22:27:34 +0900

    Update runboxpy.kv

--- runboxpy/main.py
+++ runboxpy/main.py
@@ -3,77 +3,11 @@ import traceback
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-# from kivy.uix.stacklayout import StackLayout
-from kivy.uix.widget import Widget
-from kivy.uix.behaviors import DragBehavior
-# from kivy.uix.relativelayout import RelativeLayout
-# from kivy.uix.floatlayout import FloatLayout
-from kivy.properties import StringProperty, ListProperty, ObjectProperty
+from kivy.properties import StringProperty, ObjectProperty
 from kivy.config import Config
 
 Config.set('graphics', 'width', '620')
 Config.set('graphics', 'height', '440')
-
-
-class BlockCodeBase:
-    background_color = ListProperty([1, 1, 1, 1])
-    border_color = ListProperty([1, 1, 1, 1])
-
-
-class BlockCode(Widget):
-    background_color = ListProperty([1, 1, 1, 1])
-    border_color = ListProperty([1, 1, 1, 1])
-
-
-class BlockFunction(BoxLayout, BlockCodeBase):
-    function_name = ObjectProperty(None)
-    argments = ObjectProperty(None)
-
-
-class BlockVariable(BoxLayout, BlockCodeBase):
-    variable_name = StringProperty("")
-    value = StringProperty("")
-
-
-class BlockScript(DragBehavior, BoxLayout):
-    background_color = ListProperty([1, 1, 1, 1])
-    border_color = ListProperty([1, 1, 1, 1])
-    script_name = StringProperty("")
-    code = StringProperty("")
-
-    def add_block(self, block_chooser):
-        if block_chooser.text == "print":
-            block = BlockFunction()
-            block.function_name.text = "print"
-        elif block_chooser.text == "variable":
-            block = BlockVariable()
-        else:
-            block = BlockCode()
-        self.add_widget(block)
-
-    def on_script_name(self, _, script_name):
-        self.id = script_name
-
-    def run_script(self):
-        self.code = ""
-        can_exec_code = False
-        for child_widget in reversed(self.children):
-            # Generate python code.
-            if isinstance(child_widget, BlockFunction):
-                self.code += "{}({})\n".format(
-                    child_widget.function_name.text,
-                    child_widget.argments.text)
-                can_exec_code = True
-            elif isinstance(child_widget, BlockVariable):
-                self.code += "{} = {}\n".format(
-                    child_widget.variable_name, child_widget.value)
-                can_exec_code = True
-        if can_exec_code:
-            try:
-                exec(self.code)
-            except Exception:
-                traceback.print_exc()
 
 
 class IconButton(Button):
@@ -81,10 +15,13 @@ class IconButton(Button):
 
 
 class EditScreen(Screen):
-    block_code_area = ObjectProperty(None)
+    code_input = ObjectProperty(None)
 
-    def add_script(self):
-        self.block_code_area.add_widget(BlockScript())
+    def run_code(self):
+        try:
+            exec(self.code_input.text)
+        except Exception:
+            traceback.print_exc()
 
 
 class WelcomeScreen(Screen):
