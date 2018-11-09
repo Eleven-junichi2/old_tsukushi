5d151446892cb3130f01b52015fccfe4f95b8008
Junichi Suetsugu <junichinomori@yahoo.co.jp>
2018-11-09 21:34:08 +0900

    Add file choose ui

--- runboxpy/main.py
+++ runboxpy/main.py
@@ -1,24 +1,18 @@
-from pathlib import Path
-import os
-
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
 from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem
 from kivy.properties import StringProperty
 # from kivy.garden.xpopup import XFileSave
-# from kivy.core.text import LabelBase
+from kivy.core.text import LabelBase
 from kivy.config import Config
 
 Config.set('graphics', 'width', '620')
 Config.set('graphics', 'height', '440')
 
-fonts_dir_path = Path(__file__) / Path("./fonts/")
-print(f"testtest: {fonts_dir_path.exists()}")
-# for font_dir in os.listdir(fonts_dir):
-    # for font_name in font_dir:
-        # print(font_dir, font_name)
-        # LabelBase.register(font_name, fn_bold=font_name)
+LabelBase.register("NotoSansCJKjp",
+                   fn_regular="fonts/NotoSansCJKjp/NotoSansCJKjp-Regular.otf",
+                   fn_bold="fonts/NotoSansCJKjp/NotoSansCJKjp-Bold.otf")
 
 
 class IconButton(Button):
